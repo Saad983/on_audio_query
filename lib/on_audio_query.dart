9c4d592611a1871be89a49f857b26c82df0914d6
Saad983 <137616213+Saad983@users.noreply.github.com>
2025-06-22 18:08:59 +0500

    Update on_audio_query.dart

--- lib/on_audio_query.dart
+++ lib/on_audio_query.dart
@@ -1,5 +1,13 @@
 
 // on_audio_query.dart
-library on_audio_query;
+library library on_audio_query;
 
 export 'src/on_audio_query_plugin.dart';
+export 'src/models/song_model.dart';
+export 'src/models/album_model.dart';
+export 'src/models/artist_model.dart';
+export 'src/models/genre_model.dart';
+export 'src/models/playlist_model.dart';
+export 'src/models/folder_model.dart';
+export 'src/enums/order_type.dart';
+export 'src/enums/uri_type.dart';
